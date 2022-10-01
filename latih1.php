ab9fa24289da2fb3520f7cb6e9d68cf8534d5e9c
Arrifsett <114812879+Arrifsett@users.noreply.github.com>
2022-10-01 19:42:28 +0700

    latih1
    menyimpan file latihan

---
+++ latih1.php
@@ -0,0 +1,10 @@
+<?php 
+    //contoh fungsi echo
+    $nama="arif setiawan";
+    $alamat="bekasi";
+    $a1="o";
+    $a2="k";
+    $a3="e";
+
+    echo"nama saya $nama saya tinggal di $alamat $a1$a2$a3";
+    ?>
